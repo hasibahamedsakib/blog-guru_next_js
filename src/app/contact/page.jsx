c122eefbed7c446cd05d02cd9f708bbf670b8006
Hasib Ahamed Sakib <hasibahamedsakib1@gmail.com>
2023-08-14 10:44:57 +0600

    complete contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import style from "./page.module.css";
+import contactImage from "/public/contact.png";
+import Image from "next/image";
+import Button from "../components/Button/Button";
 const ContactPage = () => {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <h1 className={style.heading}> Let`s Keep in Touch</h1>
+      <div className={style.container}>
+        <div className={style.contactImg}>
+          <Image
+            src={contactImage}
+            alt="contact image"
+            className={style.image}
+          />
+        </div>
+        <div className={style.contactForm}>
+          <form className={style.form}>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Message"
+              onResize={false}
+            ></textarea>
+            <Button url="/contact" text="Submit" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
